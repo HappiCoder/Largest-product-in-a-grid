6ca8387bc584a6b16eb5ec6c3c97a7e3e025d61f
xuelan.mei <sharonmei1219@gmail.com>
2014-11-07 13:22:15 +0800

    afterZhouYou

--- test/testBubbleSort.cpp
+++ test/testBubbleSort.cpp
@@ -1,6 +1,8 @@
 #include "CppUTest/TestHarness.h"
 #include "bubbleSort.h"
 #include "student.h"
+#include "math.h"
+using namespace std;
 
 TEST_GROUP(TestDojo) {
 };
@@ -10,18 +12,27 @@ int getNum(int *nums, int col, int i, int j) {
 }
 
 int maxProductsWithNeighbors(int * nums, int nrOfRows, int nrOfColumn) {
-	return getNum(nums, 4, 0, 0) * getNum(nums, 4, 0, 1) * getNum(nums, 4, 0, 2)
-			* getNum(nums, 4, 0, 3);
+	if (nrOfColumn == 4) {
+		return getNum(nums, 4, 0, 0) * getNum(nums, 4, 0, 1)
+				* getNum(nums, 4, 0, 2) * getNum(nums, 4, 0, 3);
+	}
+
+	int maxProduct = 0;
+	int product1 = getNum(nums, 5, 0, 0) * getNum(nums, 5, 0, 1)
+			* getNum(nums, 5, 0, 2) * getNum(nums, 5, 0, 3);
+	int product2 = getNum(nums, 5, 0, 1) * getNum(nums, 5, 0, 2)
+			* getNum(nums, 5, 0, 3) * getNum(nums, 5, 0, 4);
+	return max(product1, product2);
 }
 
 TEST(TestDojo, testOnly4NumbersInARow1) {
-	int num[1][4] = { { 1, 2, 3, 4 } };
-	int result = maxProductsWithNeighbors((int*) num, 1, 4);
-	CHECK_EQUAL(24, result);
+int num[1][4] = { { 1, 2, 3, 4 } };
+int result = maxProductsWithNeighbors((int*) num, 1, 4);
+CHECK_EQUAL(24, result);
 }
 
-TEST(TestDojo, testOnly4NumbersInARow2) {
-	int num[1][4] = { { 1, 2, 3, 5 } };
-	int result = maxProductsWithNeighbors((int*) num, 1, 4);
-	CHECK_EQUAL(30, result);
+TEST(TestDojo, testOnly5NumbersInARow) {
+int num[1][5] = { { 1, 2, 3, 5, 6 } };
+int result = maxProductsWithNeighbors((int*) num, 1, 5);
+CHECK_EQUAL(180, result);
 }
